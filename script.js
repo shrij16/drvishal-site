926fcd427f24f8216021b7adc73de8476350ede2
shrij16 <shriyaj1015@outlook.com>
2026-04-03 14:45:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ document.addEventListener("click", function (e) {
   card.classList.toggle("active");
   e.target.textContent = isOpen ? "View Details" : "Hide Details";
 });
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active'); // For animating the burger if you want
+    menuLinks.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(n => n.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}));
