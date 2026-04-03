4f27c05432e36f7da4dddba32c20512248eca873
shrij16 <shriyaj1015@outlook.com>
2026-04-03 14:56:06 +0530

    Refactor openWhatsApp and click event listener

--- script.js
+++ script.js
@@ -14,20 +14,3 @@ document.addEventListener("click", function (e) {
     }
   });
 
-  const isOpen = card.classList.contains("active");
-  card.classList.toggle("active");
-  e.target.textContent = isOpen ? "View Details" : "Hide Details";
-});
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-links');
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active'); // For animating the burger if you want
-    menuLinks.classList.toggle('active');
-});
-
-// Close menu when a link is clicked
-document.querySelectorAll('.nav-links a').forEach(n => n.addEventListener('click', () => {
-    menu.classList.remove('is-active');
-    menuLinks.classList.remove('active');
-}));
